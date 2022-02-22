6412c0c92f26bccf04bdb66515cfeddbbb54541f
alexandrelatimer <alexandre.latimer@gmail.com>
2022-02-22 15:31:08 -0500

    Added Data selection

--- model_input.py
+++ model_input.py
@@ -4,10 +4,13 @@
 	*Created Feb 2022 
 	* Authors: 
 	-Matthew Tran 300028206
-	-Alexandre Latimer 
+	-Alexandre Latimer 300027473
 
 '''
+from msilib.schema import Error
 import nibabel as nb 
+import pandas as pd
+import numpy as np
 
 '''
 Deliverable 1
@@ -18,16 +21,35 @@ Parameters - Matt (first 2) Alex (last 2)
 
 #function to check the setup file for the number of variables used  
 def check_setup(setup_name):
-	num_var=0
+	num_var=3
 	#some code to check the file and count variables ...
 	return num_var
 
 def confidence_interval_menu (bootstrap_num):
 	#code to output a menu to select confidence intervals for a certain bootstrap number 
+	return
 
 setup_name = input ("Please enter the name of the Model Setup File (make sure it's in the same directory")
 model_variables = check_setup(setup_name)
 
+# Data selection
+try:
+	data_output = pd.DataFrame()
+	data_directory = input("Enter data file with variables: ")
+	X = pd.read_csv(data_directory)
+except FileNotFoundError as e:
+	print("File '%s' could not be found" % e.filename)
+	exit()
+
+for x in range(model_variables):
+	try:
+		var = input("Select column for variable " + str(x+1) + "\n" + str(list(X)) + ": ")
+		data_output[str(x)] = X[var]
+		break
+	except KeyError as e:
+		print("Variable '%s' given is not included in data" % var)
+		exit()
+data_output.to_csv('data_output.csv', index=False)
 
 #User parameters 
 bootstrap_num = input("How many bootstraps (default 1000) do you want to include?")
